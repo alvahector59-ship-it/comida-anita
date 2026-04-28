c9d2bb45fd994135e55f495fbb51eec9ae233763
alvahector59-ship-it <alvahector59@gmail.com>
2026-04-27 19:59:35 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ventas-pro-v2';
+const CACHE='ventas-pro-v3';
 const URLS=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
